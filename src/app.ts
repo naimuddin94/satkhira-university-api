3faada04ae534b0881e9b5aec16ba0299871462d
naimuddin94 <developernaimuddin@gmail.com>
2024-05-27 21:00:08 +0600

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from 'cors';
 import express from 'express';
+import { semesterRouter } from './app/modules/semester/semester.route';
+import { userRouter } from './app/modules/user/user.route';
 import { globalErrorHandler, notFound } from './app/utils';
 const app = express();
 
@@ -11,6 +13,10 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Server is running 🎉');
 });
+
+// all routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/semesters', semesterRouter);
 
 // not found route handling
 app.all('*', notFound);
