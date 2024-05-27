2981cdd6758973590733543fcc98125eefe82e1a
naimuddin94 <developernaimuddin@gmail.com>
2024-05-28 00:10:25 +0600

    update user.validator.ts

--- src/app/modules/user/user.validator.ts
+++ src/app/modules/user/user.validator.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 export const userValidationSchema = z.object({
-  id: z.number().optional(),
+  id: z.string().optional(),
   email: z
     .string({ required_error: 'Email is required' })
     .email({ message: 'Please enter a valid email address' }),
   password: z.string().optional(),
-  needsPasswordChange: z.boolean().default(true),
+  needsPasswordChange: z.boolean().default(true).optional(),
   role: z.string().optional(),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().optional().default(false),
+  status: z.enum(['in-progress', 'blocked']).default('in-progress').optional(),
+  isDeleted: z.boolean().optional().default(false).optional(),
 });
