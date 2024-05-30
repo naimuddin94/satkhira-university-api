b24d2cdb297d5d89847d4cb805428b9464a035b3
naimuddin94 <developernaimuddin@gmail.com>
2024-05-30 16:07:21 +0600

    generate student id logic change

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,18 +1,39 @@
 import { ISemester } from '../semester/semester.interface';
 import Student from '../student/student.model';
 
-const getStudentLastId = async () => {
-  const result = await Student.findOne({}, { _id: 0, id: 1 })
+const getStudentLastId = async (query: string) => {
+  const result = await Student.findOne(
+    { id: { $regex: query } },
+    { _id: 0, id: 1 },
+  )
     .sort({ createdAt: -1 })
     .lean();
 
-  return result?.id ? Number(result.id.substring(6)) : 0;
+  return result?.id ? result.id : 0;
 };
 
+// 2030 03 0002
+
 export const generateStudentId = async (semester: ISemester) => {
-  const currentId = (await getStudentLastId()) + 1;
+  let currentId = (1).toString().padStart(4, '0');
 
-  const id = currentId.toString().padStart(4, '0');
+  const lastStudentId = (
+    await getStudentLastId(`${semester.year}${semester.code}`)
+  ).toString();
 
-  return `${semester.year}${semester.code}${id}`;
+  const lastStudentYear = lastStudentId.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId.substring(4, 6);
+  const currentStudentYear = semester.year;
+  const currentStudentSemesterCode = semester.code;
+
+  if (
+    lastStudentId &&
+    lastStudentYear === currentStudentYear &&
+    lastStudentSemesterCode === currentStudentSemesterCode
+  ) {
+    const id = Number(lastStudentId.substring(6)) + 1;
+    currentId = id.toString().padStart(4, '0');
+  }
+
+  return `${currentStudentYear}${currentStudentSemesterCode}${currentId}`;
 };
