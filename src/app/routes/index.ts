8aa2f5cf5924383fee9bcc7051984e2749c565e7
naimuddin94 <developernaimuddin@gmail.com>
2024-06-01 00:08:33 +0600

    update src/app/routes/index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { academicDepartmentRoute } from '../modules/academicDepartment/academicDepartment.route';
 import { academicFacultyRoute } from '../modules/academicFaculty/academicFaculty.route';
 import { semesterRouter } from '../modules/semester/semester.route';
+import { studentRoute } from '../modules/student/student.route';
 import { userRouter } from '../modules/user/user.route';
 
 const router = Router();
@@ -10,6 +11,10 @@ const moduleRoutes = [
   {
     path: '/users',
     route: userRouter,
+  },
+  {
+    path: '/students',
+    route: studentRoute,
   },
   {
     path: '/semesters',
