979679d34ecab145b87e2a91b84785b213addb2d
naimuddin94 <developernaimuddin@gmail.com>
2024-05-31 21:38:10 +0600

    update academicDepartment.service.ts

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -6,11 +6,21 @@ const saveAcademicDepartmentIntoDB = async (payload: IAcademicDepartment) => {
 };
 
 const fetchSingleAcademicDepartment = async (id: string) => {
-  return await AcademicDepartment.findById(id);
+  return await AcademicDepartment.findById(id).populate({
+    path: 'academicFaculty',
+    transform: (data) => {
+      return data.name;
+    },
+  });
 };
 
 const fetchAllAcademicDepartment = async () => {
-  return await AcademicDepartment.find();
+  return await AcademicDepartment.find().populate({
+    path: 'academicFaculty',
+    transform: (data) => {
+      return data.name;
+    },
+  });
 };
 
 export const academicDepartmentService = {
