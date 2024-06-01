7d1c267a698cca1e795d38336467aae99308e2d6
naimuddin94 <developernaimuddin@gmail.com>
2024-06-01 22:39:40 +0600

    update validation.ts

--- src/app/middleware/validation.ts
+++ src/app/middleware/validation.ts
@@ -1,19 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
-import { ApiError } from '../utils';
 
 export const validation =
   (schema: AnyZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result = schema.safeParse(req.body);
-      if (!result.success) {
-        throw new ApiError(
-          400,
-          result.error.issues.map((err) => err.message).join(', '),
-          result.error.issues,
-        );
-      }
+      schema.parse(req.body);
+      // const result = schema.safeParse(req.body);
+      // if (!result.success) {
+      //   throw new ApiError(
+      //     400,
+      //     result.error.issues.map((err) => err.message).join(', '),
+      //     result.error.issues,
+      //   );
+      // }
       next();
     } catch (error) {
       next(error);
