22d08d231376a85df2bb67bde8a2d43879916af4
naimuddin94 <developernaimuddin@gmail.com>
2024-06-01 22:39:47 +0600

    update student.route.ts

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express';
+import { validation } from '../../middleware/validation';
 import { studentController } from './student.controller';
+import { updateStudentValidationSchema } from './student.validator';
 
 const router = Router();
 
-router.route('/:studentId').delete(studentController.deleteStudent);
+router.route('/').get(studentController.fetchAllStudents);
+
+router
+  .route('/:studentId')
+  .delete(studentController.deleteStudent)
+  .patch(
+    validation(updateStudentValidationSchema),
+    studentController.updateStudent,
+  );
 
 export const studentRoute = router;
