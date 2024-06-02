18bf73bebcc5885fd143ca5ab240690902a61213
naimuddin94 <developernaimuddin@gmail.com>
2024-06-02 09:03:17 +0600

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,34 @@
 /* eslint-disable no-console */
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import { config } from './app/config';
+
+let server: Server;
 
 (async () => {
   try {
     await mongoose.connect(config.db_uri!);
     console.log('Database connection established 🎉');
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log('server listening on port: ' + config.port);
     });
   } catch (error) {
     console.log(error);
   }
 })();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 UnhandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
