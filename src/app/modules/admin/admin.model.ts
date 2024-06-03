97f8d056d8c31b23aae9cbdfac385cc6393e2bd2
naimuddin94 <developernaimuddin@gmail.com>
2024-06-03 15:29:30 +0600

    feat: create admin.model.ts

---
+++ src/app/modules/admin/admin.model.ts
@@ -0,0 +1,93 @@
+import { Schema, model } from 'mongoose';
+import { nameSchema } from '../student/student.model';
+import { IAdmin, IAdminModel } from './admin.interface';
+
+const adminSchema = new Schema<IAdmin, IAdminModel>(
+  {
+    id: {
+      type: String,
+      required: [true, 'ID is required'],
+      unique: true,
+    },
+    name: {
+      type: nameSchema,
+      required: [true, 'Name is required'],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'User id is required'],
+      unique: true,
+      ref: 'User',
+    },
+    designation: {
+      type: String,
+      required: [true, 'Designation is required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['male', 'female', 'other'],
+        message: '{VALUE} is not a valid gender',
+      },
+      required: [true, 'Gender is required'],
+    },
+    dateOfBirth: { type: Date },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    contactNo: { type: String, required: [true, 'Contact number is required'] },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency contact number is required'],
+    },
+    presentAddress: {
+      type: String,
+      required: [true, 'Present address is required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent address is required'],
+    },
+    profileImg: { type: String },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+// generating full name
+adminSchema.virtual('fullName').get(function () {
+  return `${this.name.firstName} ${this.name.lastName}`;
+});
+
+// filter out deleted documents
+adminSchema.pre('find', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+adminSchema.pre('findOne', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+adminSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
+//checking if user is already exist!
+adminSchema.statics.isUserExists = async function (id: string) {
+  const existingUser = await Admin.findOne({ id });
+  return existingUser;
+};
+
+export const Admin = model<IAdmin, IAdminModel>('Admin', adminSchema);
