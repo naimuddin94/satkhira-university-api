0abb031249f1daaf0660e98789f17f375db38912
naimuddin94 <developernaimuddin@gmail.com>
2024-06-03 15:29:40 +0600

    update faculty.interface.ts

--- src/app/modules/faculty/faculty.interface.ts
+++ src/app/modules/faculty/faculty.interface.ts
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { HydratedDocument, Model } from 'mongoose';
 import { z } from 'zod';
-import { facultyValidation } from './faculty.validator';
+import { facultyValidationSchema } from './faculty.validator';
 
-export interface IFaculty
-  extends z.infer<typeof facultyValidation.facultyValidationSchema> {}
+export interface IFaculty extends z.infer<typeof facultyValidationSchema> {}
 
 export interface IFacultyModel extends Model<IFaculty, Record<string, never>> {
   isFacultyExists(id: string): Promise<HydratedDocument<IFaculty>>;
