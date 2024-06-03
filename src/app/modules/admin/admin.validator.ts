98473d560699246c4abb4031d5cc5c1b1747bf3f
naimuddin94 <developernaimuddin@gmail.com>
2024-06-03 15:29:37 +0600

    update admin.validator.ts

--- src/app/modules/admin/admin.validator.ts
+++ src/app/modules/admin/admin.validator.ts
@@ -1,21 +1,35 @@
+import { Types } from 'mongoose';
 import { z } from 'zod';
 import { nameValidationSchema } from '../student/student.validator';
 
-const adminValidationSchema = z.object({
+export const adminValidationSchema = z.object({
+  id: z.string().optional(),
   designation: z.string(),
   name: nameValidationSchema,
+  userId: z
+    .preprocess((arg: unknown) => {
+      if (typeof arg === 'string') {
+        return new Types.ObjectId(arg);
+      }
+      return arg;
+    }, z.instanceof(Types.ObjectId))
+    .optional(),
+  email: z.string().email({ message: 'Invalid email' }).optional(),
   gender: z.enum(['male', 'female', 'other']),
-  dateOfBirth: z.preprocess((arg: unknown) => {
-    if (typeof arg === 'string') {
-      return new Date(arg);
-    }
-    return arg;
-  }, z.date()).optional(),
+  dateOfBirth: z
+    .preprocess((arg: unknown) => {
+      if (typeof arg === 'string') {
+        return new Date(arg);
+      }
+      return arg;
+    }, z.date())
+    .optional(),
   contactNo: z.string(),
   emergencyContactNo: z.string(),
   presentAddress: z.string(),
   permanentAddress: z.string(),
-  profileImg: z.string(),
+  profileImg: z.string().optional(),
+  isDeleted: z.boolean().optional(),
 });
 
 export const createAdminValidationSchema = z.object({
@@ -23,4 +37,3 @@ export const createAdminValidationSchema = z.object({
   password: z.string().optional(),
   admin: adminValidationSchema,
 });
-
