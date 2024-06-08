69d8a1c76c98090ad0d834d7d3a814cd6ba6ac8d
naimuddin94 <developernaimuddin@gmail.com>
2024-06-08 19:08:48 +0600

    feat: create semesterRegistration.service.ts

---
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -0,0 +1,97 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import httpStatus from 'http-status';
+import QueryBuilder from '../../builder/QueryBuilder';
+import { ApiError } from '../../utils';
+import Semester from '../semester/semester.model';
+import { RegistrationStatus } from './semesterRegistration.constant';
+import { ISemesterRegistration } from './semesterRegistration.interface';
+import SemesterRegistration from './semesterRegistration.model';
+
+const createSemesterRegistrationIntoDB = async (
+  payload: ISemesterRegistration,
+) => {
+
+  const academicSemesterId = payload?.academicSemester;
+
+  const academicSemester = await Semester.findById(academicSemesterId);
+
+  if (!academicSemester) {
+    throw new ApiError(404, 'Semester not found');
+  }
+
+  const isRunningAnySemester = await SemesterRegistration.findOne({
+    $or: [
+      { status: RegistrationStatus.ONGOING },
+      { status: RegistrationStatus.UPCOMING },
+    ],
+  });
+
+  if (isRunningAnySemester) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      `Already have ${isRunningAnySemester.status} status in another semester`,
+    );
+  }
+
+  // check if the semester is already registered!
+  const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+    academicSemester: academicSemesterId,
+  });
+
+  if (isSemesterRegistrationExists) {
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      'This semester is already registered!',
+    );
+  }
+
+  const result = await SemesterRegistration.create(payload);
+  return result;
+};
+
+const getAllSemesterRegistrationsFromDB = async (
+  query: Record<string, unknown>,
+) => {
+  const semesterRegistrationQuery = new QueryBuilder(
+    SemesterRegistration.find().populate('academicSemester'),
+    query,
+  )
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await semesterRegistrationQuery.queryModel;
+  return result;
+};
+
+const getSingleSemesterRegistrationsFromDB = async (id: string) => {
+  const result = await SemesterRegistration.findById(id);
+
+  return result;
+};
+
+const updateSemesterRegistrationIntoDB = async (
+  id: string,
+  payload: Partial<ISemesterRegistration>,
+) => {
+  const isSemesterRegistrationExists = await SemesterRegistration.findById(id);
+
+  if (!isSemesterRegistrationExists) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'This semester is not found !');
+  }
+
+  //if the requested semester registration is ended , we will not update anything
+  const currentSemesterStatus = isSemesterRegistrationExists?.status;
+  const requestedStatus = payload?.status;
+};
+
+const deleteSemesterRegistrationFromDB = async (id: string) => {};
+
+export const SemesterRegistrationService = {
+  createSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationsFromDB,
+  getSingleSemesterRegistrationsFromDB,
+  updateSemesterRegistrationIntoDB,
+  deleteSemesterRegistrationFromDB,
+};
