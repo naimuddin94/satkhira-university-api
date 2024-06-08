fc0f36b833955c26a258388fecb81e8480a20ff2
naimuddin94 <developernaimuddin@gmail.com>
2024-06-08 08:40:19 +0600

    update course.interface.ts

--- src/app/modules/course/course.interface.ts
+++ src/app/modules/course/course.interface.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { courseValidationSchema, createCourseFacultySchema, preRequisiteCourseValidationSchema } from "./course.validator";
-import { Model } from "mongoose";
+import { Model, Types } from "mongoose";
 
 export interface IPreRequisiteCourse
   extends z.infer<typeof preRequisiteCourseValidationSchema> {}
@@ -9,4 +9,6 @@ export interface ICourse extends z.infer<typeof courseValidationSchema>{ }
 
 export interface ICourseModel extends Model<ICourse>{ }
 
-export interface ICourseFaculty extends z.infer<typeof createCourseFacultySchema>{}
+export interface ICourseFaculty extends z.infer<typeof createCourseFacultySchema>{
+  course: Types.ObjectId
+}
