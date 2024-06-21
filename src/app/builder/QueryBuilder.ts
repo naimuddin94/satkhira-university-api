db8ed2631f226b8c2e760dd5ca006b72e120c3ed
naimuddin94 <developernaimuddin@gmail.com>
2024-06-21 18:53:57 +0600

    update QueryBuilder.ts

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -4,18 +4,20 @@ class QueryBuilder<T> {
   public queryModel: Query<T[], T>;
   public query: Record<string, unknown>;
 
+  // Constructor to initialize the queryModel and query object
   constructor(queryModel: Query<T[], T>, query: Record<string, unknown>) {
     this.queryModel = queryModel;
     this.query = query;
   }
 
+  // Method to handle search functionality
   search(searchableFields: string[]) {
     const searchTerm = this?.query?.searchTerm;
     if (searchTerm) {
       this.queryModel = this.queryModel.find({
         $or: searchableFields.map((field) => {
           return {
-            [field]: { $regex: searchTerm, $options: 'i' },
+            [field]: { $regex: searchTerm, $options: 'i' }, // Case-insensitive regex search
           } as FilterQuery<T>;
         }),
       });
@@ -24,10 +26,12 @@ class QueryBuilder<T> {
     return this;
   }
 
+  // Method to filter the query based on remaining fields after excluding certain ones
   filter() {
     const queryObject = { ...this.query };
     const excludeFields = ['searchTerm', 'sort', 'page', 'limit', 'fields'];
 
+    // Remove fields that should not be used for filtering
     excludeFields.forEach((field) => delete queryObject[field]);
 
     this.queryModel = this.queryModel.find(queryObject as FilterQuery<T>);
@@ -35,6 +39,7 @@ class QueryBuilder<T> {
     return this;
   }
 
+  // Method to sort the results based on the 'sort' field or default to '-createdAt'
   sort() {
     const sort = this.query?.sort || '-createdAt';
 
@@ -42,6 +47,7 @@ class QueryBuilder<T> {
     return this;
   }
 
+  // Method to paginate the results based on 'page' and 'limit' fields
   paginate() {
     const limit = Number(this?.query?.limit) || 10;
     const page = Number(this?.query?.page) || 1;
@@ -51,6 +57,8 @@ class QueryBuilder<T> {
 
     return this;
   }
+
+  // Method to select specific fields to return in the results
   fields() {
     const fields = (this?.query?.fields as string)?.split(',')?.join(' ');
 
