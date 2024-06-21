ea15c96a33d4388234bbfd66c6aae3be93dd61c8
naimuddin94 <developernaimuddin@gmail.com>
2024-06-21 18:54:22 +0600

    feat: create offeredCourse.model.ts

---
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema } from 'mongoose';
+import { Days } from './offeredCourse.constants';
+import { IOfferedCourse } from './offeredCourse.interface';
+
+const offeredCourseSchema = new mongoose.Schema<IOfferedCourse>(
+  {
+    semesterRegistration: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'SemesterRegistration',
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicSemester',
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicFaculty',
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicDepartment',
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Course',
+    },
+    faculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Faculty',
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+    },
+    section: {
+      type: Number,
+      required: true,
+    },
+    days: [
+      {
+        type: String,
+        enum: Days,
+      },
+    ],
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const OfferedCourse = mongoose.model<IOfferedCourse>(
+  'OfferedCourse',
+  offeredCourseSchema,
+);
+
+export default OfferedCourse;
