850562cbc76c8b23c3538f44dd1211bd4209ccd6
naimuddin94 <developernaimuddin@gmail.com>
2024-06-22 22:41:17 +0600

    feat: create enrolledCourse.route.ts

---
+++ src/app/modules/enrolledCourse/enrolledCourse.route.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import validateRequest from '../../middleware/validateRequest';
+import { EnrolledCourseValidations } from './enrolledCourse.validation';
+import { EnrolledCourseControllers } from './enrolledCourse.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-enrolled-course',
+  auth(USER_ROLE.student),
+  validateRequest(
+    EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
+  ),
+  EnrolledCourseControllers.createEnrolledCourse,
+);
+
+router.get(
+  '/my-enrolled-courses',
+  auth(USER_ROLE.student),
+  EnrolledCourseControllers.getMyEnrolledCourses,
+);
+
+router.patch(
+  '/update-enrolled-course-marks',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  validateRequest(
+    EnrolledCourseValidations.updateEnrolledCourseMarksValidationZodSchema,
+  ),
+  EnrolledCourseControllers.updateEnrolledCourseMarks,
+);
+
+export const EnrolledCourseRoutes = router;
